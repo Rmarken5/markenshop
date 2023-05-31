82b86e1e061a91cecc37fa6271cecf33f4d06ccc
rmarken5 <marken.ryan@gmail.com>
2023-05-31 17:24:36 -0400

    WIP: err no rows need to be returned from queries

--- webhook/internal/logic/webhook-request/controller.go
+++ webhook/internal/logic/webhook-request/controller.go
@@ -1,16 +1,21 @@
 package webhook_request
 
 import (
+	"errors"
 	"github.com/rmarken5/markenshop/database/repository"
 	pull_request "github.com/rmarken5/markenshop/webhook/internal/models/pull-request"
 	"github.com/rmarken5/markenshop/webhook/internal/models/push"
 	"github.com/rs/zerolog"
+	"io"
+	"strings"
 )
+
+const PUSH = "push"
+const PULL_REQUEST = "pull_request"
 
 type (
 	RequestController interface {
-		WritePushToDatabase(push *push.Push) error
-		WritePullRequestToDatabase(pr *pull_request.PullRequest) error
+		ProcessWebhookRequest(eventType string, push io.Reader) error
 	}
 
 	Controller struct {
@@ -19,3 +24,34 @@ type (
 		parser Parse
 	}
 )
+
+func (c *Controller) ProcessWebhookRequest(eventType string, r io.Reader) error {
+	switch strings.ToLower(eventType) {
+	case PUSH:
+		ps, err := c.parser.ToPush(r)
+		if err != nil {
+			c.logger.Err(err).Msg("parsing to push")
+			return err
+		}
+		err = c.processPush(ps)
+		if err != nil {
+			c.logger.Err(err).Msg("processing push")
+			return err
+		}
+	default:
+		return errors.New("Not Supported")
+	}
+	return nil
+}
+
+func (c *Controller) processPush(push *push.Push) error {
+
+	senderName := push.Sender.Login
+
+	return nil
+}
+
+func (c *Controller) processPullRequst(pr *pull_request.PullRequest) error {
+	// TODO: Implement me
+	panic("implement me")
+}
